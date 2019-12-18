4d77012ee28627a7787571fc189fd86ff92d2892
Mahmoud-Morgan <mhmoudmorgan@outlook.com>
2019-12-18 19:11:14 +0200

    donor thnx email,page are done

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'GadedDamak-Administration@gaded-damak.org'),
+        'name' => env('MAIL_FROM_NAME', 'Gaded Damak'),
     ],
 
     /*
