8d843026690caa58b0ac6bd9effc6028d6f1b505
Mahmoud-Morgan <mhmoudmorgan@outlook.com>
2020-01-12 00:06:12 +0200

    editing2 js links

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -11,6 +11,8 @@
 		<link rel="stylesheet" type="text/css" href="{{URL::asset('css/main.css')}}">
 		<link href="{{ asset('css/main.css') }}" rel="stylesheet">
 		<link rel="stylesheet" href="{{ secure_asset('css/main.css') }}">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 {{--        <link rel="stylesheet" type="text/css" href="{{URL::asset('css/bootstrap.css')}}">--}}
 {{--        <<link rel="stylesheet" type="text/css" href="{{URL::asset('css/bootstrap.css')}}">--}}
